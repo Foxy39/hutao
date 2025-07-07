046eed05ec1978391fce9ac5c55929401bc51340
Foxy39 <152138544+Foxy39@users.noreply.github.com>
2025-07-07 20:15:05 -0300

    Add files via upload

---
+++ comandos-termux.js
@@ -0,0 +1,18 @@
+
+
+1.
+termux-setup-storage
+
+
+2.
+pkg upgrade -y && pkg update -y && pkg install nodejs -y && pkg install nodejs-lts -y && pkg install ffmpeg -y && pkg install wget -y && pkg install git -y
+
+3.
+cd /sdcard/downloads/hutao-bot
+
+E permite.
+
+*_Após configurado, vamos pra instalação (caso você não tenha o arquivo, instale o mesmo no site do bot: https://blackmd.online/docs ):_*
+
+4.
+sh start.sh
