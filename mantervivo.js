6cbde029d6be5bbf39947608b7192bc127e98da3
Foxy39 <152138544+Foxy39@users.noreply.github.com>
2025-07-07 23:12:22 -0300

    Add files via upload

--- mantervivo.js
+++ mantervivo.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('🟢 Hutao Bot rodando com sucesso!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(PORT, () => {
+  console.log(`✅ Servidor Express rodando na porta ${PORT}`);
+});
--- start.sh
+++ start.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 startWithCode() {
-  NODE_ENV=production node --no-warnings main.js cd
+  NODE_ENV=production node --no-warnings main.js cd &
+  node mantervivo.js
 }
 
 startWithQr() {
-  NODE_ENV=production node --no-warnings main.js qr
+  NODE_ENV=production node --no-warnings main.js qr &
+  node mantervivo.js
 }
 
 defaultStart() {
-  NODE_ENV=production node --no-warnings main.js
+  NODE_ENV=production node --no-warnings main.js &
+  node mantervivo.js
 }
 
 while :
 do
-  echo -e "\033[1;33mHUTAO BOT V8.0 UPDATE 💎 ^-^\n INICIANDO, AGUARDE UM MOMENTO...🌸\033[0m"
-  if [ "$1" = "cd" ]; then
+  echo -e "\033[1;33m🔧 HUTAO BOT V8.0 UPDATE 💎\n🚀 INICIANDO, AGUARDE UM MOMENTO...\033[0m"
+
+  if [ "$1" == "cd" ]; then
     startWithCode
-  elif [ "$1" = "qr" ]; then
+  elif [ "$1" == "qr" ]; then
     startWithQr
-  else 
+  else
     defaultStart
   fi
+
   sleep 1
 done
